b7ae3c7411f3fee138f6e33c45376d483e0fbe04
Paulo Fidalgo <paulo.fidalgo.pt@gmail.com>
2023-06-16 18:53:14 +0100

    Remove timmings from RTX

--- bashrc.d/packages.bash
+++ bashrc.d/packages.bash
@@ -1,9 +1,9 @@
 #if [ -z "$RTX_ENABLED" ]; then
-  TIMEFORMAT='rtx took %R seconds.'
-  time {
+#  TIMEFORMAT='rtx took %R seconds.'
+#  time {
   export RTX_ENABLED=1
   eval "$(rtx activate bash)"
-}
+#}
 #fi
 
 # Node related configs
