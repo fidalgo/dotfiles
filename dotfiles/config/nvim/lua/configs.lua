2c6b7667b29e3735e69cc4a0ee5008c2d3b539f6
Paulo Fidalgo <paulo.fidalgo.pt@gmail.com>
2023-06-16 18:54:12 +0100

    Add shortcut to grep for a string in neovim

--- dotfiles/config/nvim/lua/configs.lua
+++ dotfiles/config/nvim/lua/configs.lua
@@ -68,6 +68,7 @@ vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<Leader>fs', builtin.grep_string, {})
 
 -- treesitter
 require'nvim-treesitter.configs'.setup {
