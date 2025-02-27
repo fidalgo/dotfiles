3f1edc6b17a7f5c1f3c5a6e28006ab504980c8ce
Paulo Fidalgo <paulo.fidalgo.pt@gmail.com>
2025-02-27 14:13:23 +0100

    Use installed rubocop

--- omaquasi/nvim/plugins/conform.lua
+++ omaquasi/nvim/plugins/conform.lua
@@ -1,18 +1,23 @@
 return {
 	"stevearc/conform.nvim",
 	opts = {
-		formatters = {
-			bundle_rubocop = {
-				command = "rubocop",
-				prepend_args = { "bundle", "exec" }, -- This adds "bundle exec" before "rubocop"
-				timeout = 50000,
-			},
-			yarn_prettier = { command = "yarn run prettier" },
-		},
+		-- 		formatters = {
+		-- 			rubocop = {
+		-- 				command = "rubocop-bundler",
+		-- 				args = {
+		-- 					"-a",
+		-- 					"-f",
+		-- 					"quiet",
+		-- 					"--stdin",
+		-- 					"$FILENAME",
+		-- 				},
+		-- 			},
+		-- 			-- 	-- 	yarn_prettier = { command = "yarn run prettier" },
+		-- 		},
 		formatters_by_ft = {
 			python = { "isort", "black" }, -- Conform will run multiple formatters sequentially
-			javascript = { "yarn_prettier", "prettier", stop_after_first = true }, -- Use a sub-list to run only the first available formatter
-			ruby = { "bundle_rubocop", "rubocop", stop_after_first = true },
+			javascript = { "prettier" }, -- Use a sub-list to run only the first available formatter
+			ruby = { "rubocop" },
 			eruby = { "htmlbeautifier", "erb-format" },
 			html = { "prettier" },
 			json = { "prettier" },
